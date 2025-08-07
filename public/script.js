207e2e73b7b63908d6a1aada277224fff5ea9047
tabassum2006 <tabassumbhavikatti74@gmail.com>
2025-08-07 12:20:55 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,19 @@
+document.getElementById("feedbackForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const message = document.getElementById("message").value;
+
+    fetch("/feedback", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name, message })
+    })
+    .then(res => res.text())
+    .then(data => {
+        alert(data); // show success message
+        document.getElementById("feedbackForm").reset();
+    });
+});
